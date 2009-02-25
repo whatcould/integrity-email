f25b90616f7ca245c33a656250683da800ba08c1
Simon Rozet <simon@rozet.name>
2009-02-25 12:31:48 +0100

    Initial formulary test rewrite

---
+++ test/mail_test.rb
@@ -0,0 +1,22 @@
+require File.dirname(__FILE__) + "/test_helper"
+
+class MailTest < Test::Unit::TestCase
+  include NotifierHelpers
+
+  def notifier
+    "Email"
+  end
+
+  def test_configuration_form
+    assert_form_have_tag "h3", :content => "SMTP Server Configuration"
+
+    assert_form_have_option "to",    "foo@example.org"
+    assert_form_have_option "from",  "bar@example.org"
+    assert_form_have_option "host",  "foobarhost.biz"
+    assert_form_have_option "user",  "foobaruser"
+    assert_form_have_option "pass",  "secret"
+    assert_form_have_option "auth",  "plain"
+    assert_form_have_option "pass",  "secret"
+    assert_form_have_option "domain","localhost"
+  end
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,78 @@
+require "test/unit"
+require "hpricot"
+require "haml"
+
+begin
+  require "redgreen"
+rescue LoadError
+end
+
+require File.dirname(__FILE__) + "/../lib/notifier/email"
+
+module HpricotAssertions
+  # Thanks Harry! http://gist.github.com/39960
+  class HpricotMatcher
+    def initialize(html)
+      @doc = Hpricot(html)
+    end
+
+    # elements('h1') returns a Hpricot::Elements object with all h1-tags.
+    def elements(selector)
+      @doc.search(selector)
+    end
+
+    # element('h1') returns Hpricot::Elem with first h1-tag, or nil if none exist.
+    def element(selector)
+      @doc.at(selector)
+    end
+
+    # tags('h1') returns the inner HTML of all matched elements mathed.
+    def tags(selector)
+      e = elements(selector)
+      e.map {|x| x.inner_html}
+    end
+
+    # tag('h1') returns the inner HTML of the first mached element, or nil if none matched.
+    def tag(selector)
+      e = element(selector)
+      e && e.inner_html
+    end
+  end
+
+  def assert_have_tag(html, selector, content=nil)
+    matcher = HpricotMatcher.new(html)
+    assert_equal content, matcher.tag(selector) if content
+    assert matcher.tag(selector)
+  end
+end
+
+module NotifierFormHelpers
+  include HpricotAssertions
+
+  def form(config={})
+    Haml::Engine.new(Integrity::Notifier::Email.to_haml).
+      render(OpenStruct.new(:config => config))
+  end
+
+  def assert_form_have_tag(selector, options={})
+    content = options.delete(:content)
+    assert_have_tag(form(options), selector, content)
+  end
+
+  def assert_form_have_option(option, value=nil)
+    selector = "input##{notifier.downcase}_notifier_#{option}"
+    selector << "[@name='notifiers[#{notifier}][#{option}]']"
+    selector << "[@value='#{value}']" if value
+
+    assert_form_have_tag(selector, option => value)
+  end
+
+  def assert_form_have_options(*options)
+    options.each { |option| assert_form_have_option(option) }
+  end
+end
+
+module NotifierHelpers
+  include NotifierFormHelpers
+end
+
